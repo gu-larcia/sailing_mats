80320b94e5e34593bd2cedced2d7464c1bfbe0cc
Lu Garcia <luisgdlc94@gmail.com>
2026-03-18 18:38:21 -0400

    Reapply "initial build"
    This reverts commit 916502eba71bb2ffd119b752442300b2801a626d.

--- config/__init__.py
+++ config/__init__.py
@@ -3,21 +3,25 @@
 from .settings import (
     APP_VERSION,
     APP_TITLE,
+    APP_SUBTITLE,
     APP_ICON,
     CACHE_TTL_PRICES,
     CACHE_TTL_MAPPING,
     CACHE_TTL_CHAINS,
     DEFAULT_CONFIG,
     URL_PARAMS,
+    ITEM_GROUPS,
 )
 
 __all__ = [
     'APP_VERSION',
     'APP_TITLE',
+    'APP_SUBTITLE',
     'APP_ICON',
     'CACHE_TTL_PRICES',
     'CACHE_TTL_MAPPING',
     'CACHE_TTL_CHAINS',
     'DEFAULT_CONFIG',
     'URL_PARAMS',
+    'ITEM_GROUPS',
 ]
